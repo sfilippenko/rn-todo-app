09bf18876ad3a2db9adfba24e2c28839384fe3c0
s.filippenko <s.filippenko@savetime.net>
2021-02-15 14:01:09 +0300

    eslint + prettier

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
